3045bba4effe10789220af4bc6b24705190e53bd
DuncanMcClellan <54558786+DuncanMcClellan@users.noreply.github.com>
2020-03-06 12:47:08 -0500

    Create languages.h

---
+++ languages.h
@@ -0,0 +1,10 @@
+#ifndef LANGS_H_INCLUDED
+#define LANGS_H_INCLUDED
+
+#include <list>
+#include <iterator>
+
+std::list<string> allLangs;
+string langs[] = {"Abyssal", "Celestial", "Draconic", "Deep Speech", "Infernal", "Primodial", "Sylvan", "Undercommon", "Elvish", "Giant", "Dwarvish", "Gnomish", "Goblin", "Halfling", "Orc"};
+
+#endif
