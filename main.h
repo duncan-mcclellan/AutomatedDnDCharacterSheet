a5003a416cf77a5365cc6a246371c123ea71a058
DuncanMcClellan <54558786+DuncanMcClellan@users.noreply.github.com>
2020-03-06 12:47:23 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#define D6 6
+
+//Function Prototypes
+int* fillStats();
+
+//Global variables used for clarity
+enum order {str, dex, con, intel, wis, cha};
+std::string stat[] = {"str", "dex", "con", "intel", "wis", "cha"};
+
+#endif
